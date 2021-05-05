061f5b06dcb49343bb75ea45e14285aec6a1b748
f.sousa <ng.dev.sousa@outlook.com>
2021-05-05 11:34:55 -0300

    Add yup resolver

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../components/Form/Input";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 interface SignInFormData {
   email: string;
   password: string;
 }
 
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required("Email required.").email(),
+  password: yup.string().required("Password required.")
+});
+
 export default function Home() {
-  const { register, formState, handleSubmit } = useForm();
+  const { register, formState, handleSubmit } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  });
   const { errors } = formState;
 
   const handleSignIn: SubmitHandler<SignInFormData> = (formData, event) => {};
@@ -31,14 +40,14 @@ export default function Home() {
             type="email"
             label="email"
             error={errors.email}
-            {...register("email", { required: "Email required." })}
+            {...register("email")}
           />
           <Input
             name="password"
             type="password"
             label="password"
             error={errors.password}
-            {...register("password", { required: "Password required." })}
+            {...register("password")}
           />
           <Button
             type="submit"
